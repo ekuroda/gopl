4f3caf0a75b7314122cc12132d6bb86de0e9203a
ekuroda <kurodaeishi@gmail.com>
2020-10-04 13:36:54 +0900

    ex7.13

---
+++ ch7/ex7.13/ast.go
@@ -0,0 +1,28 @@
+package eval
+
+// Expr ...
+type Expr interface {
+	Eval(env Env) float64
+	Check(vars map[Var]bool) error
+	String() string
+}
+
+// Var ...
+type Var string
+
+type literal float64
+
+type unary struct {
+	op rune // one of '+', '-'
+	x  Expr
+}
+
+type binary struct {
+	op   rune // one of '+', '-', '*', '/'
+	x, y Expr
+}
+
+type call struct {
+	fn   string // one of "pow", "sin", "sqrt"
+	args []Expr
+}
