6264fbedd895f2e5c9e4906766a9d51a66b7b2d8
ekuroda <kurodaeishi@gmail.com>
2020-12-26 19:51:36 +0900

    ex13.3

---
+++ ch13/ex13.3/bzip/bzip2.c
@@ -0,0 +1,19 @@
+#include <bzlib.h>
+#include <stdlib.h>
+
+bz_stream* bz2alloc() { return calloc(1, sizeof(bz_stream)); }
+
+int bz2compress(bz_stream *s, int action,
+    char *in, unsigned *inlen, char *out, unsigned *outlen) {
+    s->next_in = in;
+    s->avail_in = *inlen;
+    s->next_out = out;
+    s->avail_out = *outlen;
+    int r = BZ2_bzCompress(s, action);
+    *inlen -= s->avail_in;
+    *outlen -= s->avail_out;
+    s->next_in = s->next_out = NULL;
+    return r;
+}
+
+void bz2free(bz_stream* s) { free(s); }
